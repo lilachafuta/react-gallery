9029ada60f64937a58b3678b26261110914b0b11
Lilach Afuta <lilachafuta92@gmail.com>
2019-11-13 20:48:36 +0200

    gallery with animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import React from 'react';
 import './App.css';
+import styled, { keyframes } from 'styled-components';
+import { rubberBand } from 'react-animations';
+import { CSSTransition } from "react-transition-group";
+
+const fadeInAnimation = keyframes`${rubberBand}`;
+const FadeInDiv = styled.div`
+  animation: 2s ${fadeInAnimation};
+`;
 
 class App extends React.Component {
-
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            albums : [],
+            photos :[],
+            appearHome: true
+        };
+    }
+    toggleAppear = () => {
+        this.setState({
+            appearHome: !this.state.appearHome
+        })
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/albums')
+            .then(res => res.json())
+            .then(albums => {
+                this.setState({albums});
+            });
+    }
+    getPhotos(event) {
+        fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${event.target.value}`)
+            .then(res => res.json())
+            .then(photos => {
+                this.setState({photos});
+            });
+    }
+    render() {
     return (
         <div className="App">
-          <h1>Select an album:</h1>
-
+            <FadeInDiv><h1>Gallery App 🎞</h1></FadeInDiv>
+            <select className="albums" onChange={this.getPhotos.bind(this)} onSelect={() => this.toggleAppear()}>>
+                <option>Choose your favorite album 💗️</option>
+                    {this.state.albums.map(album => {
+                        return <option value={album.id} key={album.id}>
+                            {album.title}
+                        </option>
+                    })}
+            </select>
+            <br/>
+            <br/>
+            <br/>
+                <div className="photos">
+                    {this.state.photos.map(photo => {
+                        return <CSSTransition
+                            in={true}
+                            appear={true}
+                            timeout={1000}
+                            classNames="fade">
+                            <img src={photo.thumbnailUrl} alt={photo.title} key={photo.id}/>
+                        </CSSTransition>
+                    })}
+                </div>
         </div>
     );
   }
